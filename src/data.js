36d04b5d6947105044ff8ef168c8b685a82dc337
raytieu <ray.tieu@gmail.com>
2022-07-28 16:17:44 -0700

    finished data.js

--- src/data.js
+++ src/data.js
@@ -61,7 +61,26 @@ function chillHop() {
       id: uuidv4(),
       active: false,
     },
-    //ADD MORE HERE
+    {
+      name: "Jazz Cabbage",
+      artist: "Ian Ewing, Strehlow",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["BA4A46", "#FDF0DD"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9363",
+    },
+    {
+      name: "Lagoons",
+      artist: "Strehlow, Chris Mazuera",
+      id: uuidv4(),
+      active: false,
+      color: ["BA4A46", "#FDF0DD"],
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8266",
+    },
   ];
 }
 
